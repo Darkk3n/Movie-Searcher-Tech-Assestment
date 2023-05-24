f408d2308afc15561024975cc8cbadfefa8b7d98
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-05-24 16:48:20 -0600

    First step. Rendering results from a JSON file coming from the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import "./App.css";
 
+import responseMovies from "./mocks/with-results.json";
+
 function App() {
-	return <h1>Hello World</h1>;
+	const movies = responseMovies.Search;
+	const hasMovies = movies?.length > 0;
+	return (
+		<div className="page">
+			<header>
+				<h1>Movie Searcher</h1>
+				<form className="form">
+					<input placeholder="Avengers, Star Wars, The Matrix..." />
+					<button type="submit">Search</button>
+				</form>
+			</header>
+			<main>
+				{hasMovies ? (
+					<ul>
+						{movies.map((m) => (
+							<li key={m.imdbID}>
+								<h3>{m.Title}</h3>
+								<p>{m.Year}</p>
+								<img src={m.Poster} alt={m.Title} />
+							</li>
+						))}
+					</ul>
+				) : (
+					<p>No movies found</p>
+				)}
+			</main>
+		</div>
+	);
 }
 
 export default App;
